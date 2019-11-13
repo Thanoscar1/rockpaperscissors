e2d045db05667371b005fa245487c93359b4a584
Thanoscar1 <jdonell62@gmail.com>
2019-11-13 23:26:46 +0000

    yah boi

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
+$("#shoot").click(function() {
+    // Create a variable called 'fortune' that generates a random number
+let computerchoices = Math.random();
 
-//GLOBAL VARIABLES
-/* global $ */
+    console.log(computerchoices);
+    if (computerchoices = "rock"  && computerchoices == "rock" ); {
+        $("#text").html("Nice try but its a tie");
+    if (computerchoices = "paper"  && computerchoices == "paper" ); {
+     $("#text").html("Nice try but its a tie");
+    if (computerchoices = "scissors"  && computerchoices == "scissor" ); {
+        $("#text").html("Nice try but its a tie");
+    if (computerchoices = "scissors"  && computerchoices == "rock" ); {
+     $("#text").html("Oh no u won");
+     if (computerchoices = "scissors"  && computerchoices == "paper" ); {
+        $("#text").html("yeah I won!");
+    if (computerchoices = "rock"  && computerchoices == "scissor" ); {
+        $("#text").html("yeah I won");
+        
 
+        
+    }else if (fortune < 0.66) {
+      $("#text").html("wall");
+    } else {
+      $("#text").html("Go away");
+    }
 
-
-// DOCUMENT READY FUNCTION BELOW
-
+});
