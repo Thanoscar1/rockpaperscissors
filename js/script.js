c0ad41cbba1725f2f00df90444b9d097dcabd158
Thanoscar1 <jdonell62@gmail.com>
2019-11-20 22:39:18 +0000

    haha funny

--- js/script.js
+++ js/script.js
@@ -1,27 +1,40 @@
 $("#shoot").click(function() {
+let computerchoice = Math.random();
+let userchoice = $("#input").val();
+let randomNumber = Math.random();
+
+$("#userchoice").html(userchoice);
+$("#computerChoice").html(computerChoice);
     // Create a variable called 'fortune' that generates a random number
-let computerchoices = Math.random();
 
-    console.log(computerchoices);
-    if (computerchoices = "rock"  && computerchoices == "rock" ); {
+if(randomNumber > 0 && randomNumber < .4){
+    computerChoice = "paper"
+}
+if(randomNumber > .4 && randomNumber < .7){
+    computerChoice = "rock"
+}
+if(randomNumber > .7 && randomNumber < 1){
+    computerChoice = "scissors"
+}
+    if (computerChoice == "rock"  && userchoice == "rock" ) {
         $("#text").html("Nice try but its a tie");
-    if (computerchoices = "paper"  && computerchoices == "paper" ); {
+    } if (computerChoice == "paper"  && userchoice == "paper" ) {
      $("#text").html("Nice try but its a tie");
-    if (computerchoices = "scissors"  && computerchoices == "scissor" ); {
+    } if (computerChoice == "scissors"  && userchoice == "scissor" ) {
         $("#text").html("Nice try but its a tie");
-    if (computerchoices = "scissors"  && computerchoices == "rock" ); {
+    } if (computerChoice == "scissors"  && userchoice == "rock" ) {
      $("#text").html("Oh no u won");
-     if (computerchoices = "scissors"  && computerchoices == "paper" ); {
+    } if (computerChoice =="scissors"  && userchoice == "paper" ) {
         $("#text").html("yeah I won!");
-    if (computerchoices = "rock"  && computerchoices == "scissor" ); {
+    } if (computerChoice == "rock"  && userchoice == "scissor" ) {
         $("#text").html("yeah I won");
-        
-
-        
-    }else if (fortune < 0.66) {
-      $("#text").html("wall");
+    } if (computerChoice == "rock"  && userchoice == "paper" ) {
+        $("#text").html("oh no u won");
+    } if (computerChoice == "paper"  && userchoice == "rock" ) {
+        $("#text").html("yeah I won");
+    } if (computerChoice == "paper"  && userchoice == "scissor" ) {
+        $("#text").html("oh no u won");
     } else {
-      $("#text").html("Go away");
+        $("#text").html("HAHA! funny,W A L L");
     }
-
 });
